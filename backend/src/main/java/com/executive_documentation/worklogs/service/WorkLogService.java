4b93fe42499b27584f21dfdd10cda4ad4d7a5950
thedge19 <paz80@yandex.ru>
2025-06-06 13:24:26 +0300

    feat/ add workLog #2

--- backend/src/main/java/com/executive_documentation/worklogs/service/WorkLogService.java
+++ backend/src/main/java/com/executive_documentation/worklogs/service/WorkLogService.java
@@ -3,7 +3,6 @@ package com.executive_documentation.worklogs.service;
 import com.executive_documentation.acts.dto.ActLogResponseDto;
 import com.executive_documentation.worklogs.dto.WorkLogDto;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface WorkLogService {
