57abeea8042fd3b7288b6f19a4a13bbb397c090e
thedge19 <paz80@yandex.ru>
2025-06-20 18:27:03 +0300

    feat/ add auth2#4

--- backend/src/main/java/com/executive_documentation/auth/config/JwtAuthEntryPoint.java
+++ backend/src/main/java/com/executive_documentation/auth/config/JwtAuthEntryPoint.java
@@ -1,6 +1,5 @@
 package com.executive_documentation.auth.config;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
